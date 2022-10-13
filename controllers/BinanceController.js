9c9d2bad143f1e1c0da45341a78f522b9b44d7ae
Eber Reta <eber.retabaeza@gmail.com>
2022-10-13 16:43:08 -0600

    Top Losses without limit test

--- controllers/BinanceController.js
+++ controllers/BinanceController.js
@@ -10,7 +10,7 @@ const BinanceController = {
     topLosses: async (limit, type) => {
         const response = await binanceClient.ticker24hr(undefined, undefined, type)
         const topLosses = response.data.sort((a, b) => a.priceChangePercent - b.priceChangePercent)
-        const topLossesWithLimit = topLosses.slice(0, limit ? limit : currencies.length);
+        const topLossesWithLimit = topLosses.slice(0, limit ? limit : topLosses.length);
         return topLossesWithLimit;
     },
 
--- tests/controllers/BinanceController.test.js
+++ tests/controllers/BinanceController.test.js
@@ -1,8 +1,14 @@
 const BinanceController = require('../../controllers/BinanceController');
 
-test('Get Account Info', async () => {
+test('Get Account Info ¿¿', async () => {
     const data = await BinanceController.accountInfo();
     expect(data).toBeDefined()
     expect(data.balances).toBeDefined()
     expect(data.permissions).toBeDefined()
+});
+
+test('Get Top losses without limit should retrieve 20 items', async () => {
+    const data = await BinanceController.topLosses();
+    expect(data).toBeDefined();
+    expect(data.length).toBe(20);
 });
